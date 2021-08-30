582ae2d9d2c1f74f29475e9a104498380e46df5a
austinrutledge0 <austinrutledge0@gmail.com>
2021-08-29 21:53:41 -0700

    added vue router

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <HomePage msg="Welcome to Your Vue.js App"/>
+  <RouterView/>
 </template>
 
 <script>
-import HomePage from './components/HomePage.vue'
+
 import './styles/global.css'
 
 export default {
   name: 'App',
   components: {
-    HomePage
+
   }
 }
 </script>
---
+++ src/router.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import HomePage from './components/HomePage.vue'
+
+
+
+export const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: '/',
+      component: HomePage,
+    },
+    {
+      path: '/test',
+      component: HomePage,
+    },
+  ],
+})
+
+export const routerPush = (name, params) => {
+  if (params !== undefined) {
+    return router.push({
+      name,
+      params,
+    })
+  } else {
+    return router.push({ name })
+  }
+}
